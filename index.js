5f3159faf835789d478eebc21b118538e1f1c0b1
rusty619 <rusty6916@gmail.com>
2022-03-15 10:09:33 -0400

    commit tree 0c55669496e66a330fd428b4abe9c378d7a4094b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'Ruslan'
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage  = welcomeMessage.toUpperCase();
+
+let user = currentUser[0]
+
+let shortGreeting = `Welcome, ${user}!`
